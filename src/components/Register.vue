ed8b75cd291114a7e27d3df4faaaf64eb4882ed8
onion <969023014@qq.com>
2019-06-05 15:50:28 +0800

    initial project

---
+++ src/components/Register.vue
@@ -0,0 +1,183 @@
+<template>
+  <el-form :model="registerForm" :rules="rules" ref="registerForm" class="login-container" label-position="left"
+           label-width="20px" v-loading="loading">
+    <h3 class="login_title">注册</h3>
+
+    <el-form-item prop="email">
+      <el-input type="text" v-model="registerForm.email" auto-complete="off" placeholder="邮箱"></el-input>
+    </el-form-item>
+
+    <el-form-item prop="nickname">
+      <el-input type="text" v-model="registerForm.nickname" auto-complete="off" placeholder="昵称"></el-input>
+    </el-form-item>
+
+    <el-form-item prop="password">
+      <el-input type="password" v-model="registerForm.password" auto-complete="off" placeholder="密码"></el-input>
+    </el-form-item>
+
+    <el-form-item prop="checkPassword">
+      <el-input type="password" v-model="registerForm.checkPassword" auto-complete="off" placeholder="确认密码"></el-input>
+    </el-form-item>
+
+    <el-row>
+    <el-col :span="12">
+    <el-form-item prop="code">
+      <el-input type="text" v-model="registerForm.code" auto-complete="off" placeholder="验证码"></el-input>
+    </el-form-item>
+    </el-col>
+
+    <el-col :span="12">
+    <el-form-item style="width: 100%">
+      <el-button @click.native.prevent="sendCode('registerForm')" :disabled="this.timer != null">{{msg}}</el-button>
+    </el-form-item>
+    </el-col>
+    </el-row>
+
+    <el-form-item style="width: 100%">
+      <el-button type="primary" @click="submitForm('registerForm')" style="width: 100%">注册</el-button>
+    </el-form-item>
+
+
+
+  </el-form>
+</template>
+
+<script>
+  import {getRequest} from '../utils/api'
+  import {postRequest} from "../utils/api";
+  export default{
+    data(){
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.registerForm.checkPassword !== '') {
+            this.$refs.registerForm.validateField('checkPassword');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.registerForm.password) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
+      return {
+        registerForm: {
+          email: '',
+          nickname: '',
+          password: '',
+          checkPassword: '',
+          code: '',
+        },
+        rules: {
+          email: [{required: true, message: '请输入邮箱', trigger: 'blur'},
+            {type: 'email', message: '请输入合法邮箱', trigger: 'blur'}],
+          nickname: [{required: true, message: '请输入昵称', trigger: 'blur'},
+            {min: 2, max: 16, message: '长度为3-16个字符', trigger: 'blur'}],
+          password: [
+            { validator: validatePass, trigger: 'blur' },
+            {pattern: /[a-zA-Z0-9]{6,16}/, message:'密码为6-16位的字母或数字', trigger: 'blur'}
+          ],
+          checkPassword: [
+            { validator: validatePass2, trigger: 'blur' }
+          ],
+          code: [
+            {required: true, message: '请输入验证码', trigger: 'blur'},
+            {length: 6, message: '请输入6位验证码', trigger: 'blur'}
+          ]
+        },
+        msg: '发送验证码',
+        timer: null,
+        count: ''
+      }
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            let _this = this;
+            postRequest('/user/register',{
+              email: this.registerForm.email,
+              nickname: this.registerForm.nickname,
+              password: this.registerForm.password
+            },{checkcode: this.registerForm.code}
+            ).then(resp => {
+              if (resp.data.code === 0) {
+                _this.$alert('验证码已发送');
+              } else {
+                _this.$alert('验证码发送失败');
+              }
+            },resp=> {
+              _this.$alert('服务器繁忙');
+            });
+            this.$alert('您已成功注册');
+          } else {
+            this.$alert('请正确填写注册信息');
+            return false;
+          }
+        });
+      },
+      sendCode: function (formName) {
+        this.$refs[formName].validateField('email',(valid) =>{
+          window.alert(valid);
+          if(valid){
+            this.count = 60;
+            this.timer = setInterval(() => {
+              if (this.count > 0) {
+                this.count--;
+                this.msg = this.count + 's后再次发送'
+              } else {
+                clearInterval(this.timer);
+                this.timer = null;
+                this.msg = '发送验证码';
+              }
+            }, 1000);
+            let _this = this;
+            getRequest('/user/sendCode', {
+              email: this.registerForm.email
+            }).then(resp => {
+              if (resp.data.code === 0) {
+                _this.$alert('验证码已发送');
+              } else {
+                _this.$alert('验证码发送失败');
+              }
+            },resp=> {
+              _this.$alert('服务器繁忙');
+            });
+          }else{
+            this.$alert('请正确填写邮箱')
+          }
+        });
+      },
+    }
+  }
+</script>
+
+<style scoped>
+  .login-container {
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 180px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+  }
+
+  .login_title {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+  }
+
+  .login_remember {
+    margin: 0px 0px 35px 0px;
+    text-align: left;
+  }
+</style>
---
+++ src/utils/api.js
@@ -0,0 +1,61 @@
+import axios from 'axios'
+
+let base = 'http://localhost:8080/dxs';
+export const postRequest = (url, data, params) => {
+  return axios({
+    method: 'post',
+    url: `${base}${url}`,
+    data: data,
+    params: params,
+    transformRequest: [function (data) {
+      data = JSON.stringify(data);
+      return data
+    }],
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
+export const uploadFileRequest = (url, params) => {
+  return axios({
+    method: 'post',
+    url: `${base}${url}`,
+    data: params,
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  });
+}
+export const putRequest = (url, params) => {
+  return axios({
+    method: 'put',
+    url: `${base}${url}`,
+    data: params,
+    transformRequest: [function (data) {
+      let ret = ''
+      for (let it in data) {
+        ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+      }
+      return ret
+    }],
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  });
+}
+export const deleteRequest = (url) => {
+  return axios({
+    method: 'delete',
+    url: `${base}${url}`
+  });
+}
+export const getRequest = (url,params) => {
+  return axios({
+    method: 'get',
+    url: `${base}${url}`,
+    params: params,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  });
+}
