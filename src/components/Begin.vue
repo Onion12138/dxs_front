96ee242af2ecf1ca75eda7717062e9f941af6f2c
LEO D PEN <33611404+LEODPEN@users.noreply.github.com>
2019-06-13 22:37:08 +0800

    :fire: :fire:

---
+++ src/components/Begin.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div class="app-container">
+    <h4 style="text-align: center">To Begin !</h4>
+    <img src="https://mpy634.cn/img/ecnu.jpg" alt="ECNU">
+    <h4 style="alignment-baseline: baseline ; text-align: left" >仓库地址：</h4>
+    <div style="display: flex;justify-content: space-around;flex-wrap: wrap">
+  <el-card style="width:444px;margin-top: 10px; background-color: bisque">
+    <div style="text-align: left">
+      <p>前端： <a href="https://github.com/Onion12138/dxs_front">https://github.com/Onion12138/dxs_front</a></p>
+<!--      <p>部分接口文档：<a href="https://easydoc.xyz/?#/doc/31470209/7JDeGvGf">https://easydoc.xyz/?#/doc/31470209/7JDeGvGf</a></p>-->
+    </div>
+  </el-card>
+      <el-card style="width:444px;margin-top: 10px; background-color: bisque">
+        <div style="text-align: left">
+          <p>后端： <a href="https://github.com/LEODPEN/DaXueShi">https://github.com/LEODPEN/DaXueShi</a></p>
+          <!--      <p>部分接口文档：<a href="https://easydoc.xyz/?#/doc/31470209/7JDeGvGf">https://easydoc.xyz/?#/doc/31470209/7JDeGvGf</a></p>-->
+        </div>
+      </el-card>
+  </div>
+  </div>
+</template>
+
+
