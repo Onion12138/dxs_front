c1d8acf68e395c568597981289f26948b9016311
lly <1873231395@qq.com>
2019-06-14 19:36:40 +0800

    add year selection

--- src/components/MultiChart.vue
+++ src/components/MultiChart.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="chart-region">
     <div class="title">当年应届生毕业后选择</div>
-    <br>
+    <el-select v-model="value" placeholder="请选择年份" style="margin-top:10px;width:110px">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+        size="mini">
+      </el-option>
+    </el-select>
+    <br><br>
     <button @click="changeType">切换图表类型</button>
     <vechart :data="chartData" :legend-visible="false" :settings="chartSetting"></vechart>
   </div>
@@ -29,7 +38,29 @@
         },
         chartSetting : {
           type: this.typeArr[this.index]
-        }
+        },
+        options: [{
+          value: '2019'
+        }, {
+          value: '2018'
+        }, {
+          value: '2017'
+        }, {
+          value: '2016'
+        }, {
+          value: '2015'
+        }, {
+          value: '2014'
+        }, {
+          value: '2013'
+        }, {
+          value: '2012'
+        }, {
+          value: '2011'
+        }, {
+          value: '2010'
+        }],
+        value: '2019'
       }
     },
     methods: {
@@ -53,6 +84,25 @@
           _this[1].sum = res.data.data.work;
           _this[2].sum = res.data.data.abroad;
           _this[3].sum = res.data.data.unemployment;
+        }else{
+          this.$alert("加载失败");
+        }
+      })
+    },
+    updated() {
+      //this.$alert(this.value);
+      postRequest('/student/salaryChange', {
+        "college": sessionStorage.getItem("universityName"),
+        "major": sessionStorage.getItem("majorName"),
+        "year": this.value,
+        "token": sessionStorage.getItem("token"),
+      }).then(res => {
+        if (res.data.code === 0) {
+          let _this=this.chartData.rows;
+          for(let i = 0;i < res.data.data.salary.length;i = i+1) {
+            _this[i].year = res.data.data.year[i]+"";
+            _this[i].salary = res.data.data.salary[i]+"";
+          }
         }else{
           this.$alert("加载失败");
         }
