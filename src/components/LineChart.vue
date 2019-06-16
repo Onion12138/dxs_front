e6cf3a30e2024d1b351164601ff2d39750bdd9ec
lly <1873231395@qq.com>
2019-06-16 18:09:50 +0800

    fix2

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,15 +1,6 @@
 <template>
   <div>
     <div class="title">近五年平均薪资</div>
-    <el-select v-model="value" placeholder="请选择年份" style="margin-top:10px;width:110px">
-      <el-option
-        v-for="item in options"
-        :key="item.value"
-        :label="item.label"
-        :value="item.value"
-        size="mini">
-      </el-option>
-    </el-select>
     <div class="chart-region">
       <ve-line :data="chartData" :legend-visible="false" :extend="extend"></ve-line>
     </div>
@@ -40,29 +31,7 @@
             { 'year': '', 'salary': ''},
             { 'year': '', 'salary': ''}
           ]
-        },
-        options: [{
-          value: '2019'
-        }, {
-          value: '2018'
-        }, {
-          value: '2017'
-        }, {
-          value: '2016'
-        }, {
-          value: '2015'
-        }, {
-          value: '2014'
-        }, {
-          value: '2013'
-        }, {
-          value: '2012'
-        }, {
-          value: '2011'
-        }, {
-          value: '2010'
-        }],
-        value: '2019'
+        }
       }
     },
     components: { VeLine },
@@ -71,25 +40,6 @@
         "college": sessionStorage.getItem("universityName"),
         "major": sessionStorage.getItem("majorName"),
         "year": 2019,
-        "token": sessionStorage.getItem("token"),
-      }).then(res => {
-        if (res.data.code === 0) {
-          let _this=this.chartData.rows;
-          for(let i = 0;i < res.data.data.salary.length;i = i+1) {
-            _this[i].year = res.data.data.year[i]+"";
-            _this[i].salary = res.data.data.salary[i]+"";
-          }
-        }else{
-          this.$alert("加载失败");
-        }
-      })
-    },
-    updated() {
-      //this.$alert(this.value);
-      postRequest('/student/salaryChange', {
-        "college": sessionStorage.getItem("universityName"),
-        "major": sessionStorage.getItem("majorName"),
-        "year": this.value,
         "token": sessionStorage.getItem("token"),
       }).then(res => {
         if (res.data.code === 0) {
